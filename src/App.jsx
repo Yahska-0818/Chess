82a16c996d9789a9eaf9ca9c03a6ccf1d63fe62f
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-10-29 16:57:22 +0000

    added check

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ function App() {
   const [winner, setWinner] = useState("")
   const [enPassantTarget, setEnPassantTarget] = useState(null)
   const [promotionData, setPromotionData] = useState(null)
+  const [inCheck, setInCheck] = useState(null)
 
   useEffect(() => {
     const boardCopy = chessBoard.map(row => [...row])
@@ -52,13 +53,19 @@ function App() {
     }
   }, [blackPieces, whitePieces]);
 
-  const getColor = (coords) => {
-    const color = chessBoard[coords[0]][coords[1]][0].props.className.slice(0, 5)
+  const getColor = (coords, board) => {
+    const b = board || chessBoard;
+    const cell = b[coords[0]][coords[1]];
+    if (!cell) return null;
+    const color = cell[0].props.className.slice(0, 5)
     return color
   }
 
-  const getType = (coords) => {
-    const className = chessBoard[coords[0]][coords[1]][0].props.className;
+  const getType = (coords, board) => {
+    const b = board || chessBoard;
+    const cell = b[coords[0]][coords[1]];
+    if (!cell) return null;
+    const className = cell[0].props.className;
     const type = className.split(' ')[0].split('-')[1];
     return type;
   }
@@ -78,6 +85,7 @@ function App() {
     setWinner("");
     setEnPassantTarget(null);
     setPromotionData(null);
+    setInCheck(null);
   };
 
   const ROOK_DIRECTIONS = [
@@ -101,17 +109,17 @@ function App() {
   ];
   const KING_OFFSETS = QUEEN_DIRECTIONS;
 
-  const getSlidingMoves = (r, c, pieceColor, directions) => {
+  const getSlidingMoves = (r, c, pieceColor, directions, board) => {
     const moves = [];
     for (const [dr, dc] of directions) {
       let newRow = r + dr;
       let newCol = c + dc;
 
       while (isValidPosition(newRow, newCol)) {
-        const targetSquare = chessBoard[newRow][newCol];
+        const targetSquare = board[newRow][newCol];
 
         if (targetSquare) {
-          if (getColor([newRow, newCol]) !== pieceColor) {
+          if (getColor([newRow, newCol], board) !== pieceColor) {
             moves.push([newRow, newCol]);
           }
           break;
@@ -126,15 +134,15 @@ function App() {
     return moves;
   };
 
-  const getSteppingMoves = (r, c, pieceColor, offsets) => {
+  const getSteppingMoves = (r, c, pieceColor, offsets, board) => {
     const moves = [];
     for (const [dr, dc] of offsets) {
       const newRow = r + dr;
       const newCol = c + dc;
 
       if (isValidPosition(newRow, newCol)) {
-        const targetSquare = chessBoard[newRow][newCol];
-        if (!targetSquare || getColor([newRow, newCol]) !== pieceColor) {
+        const targetSquare = board[newRow][newCol];
+        if (!targetSquare || getColor([newRow, newCol], board) !== pieceColor) {
           moves.push([newRow, newCol]);
         }
       }
@@ -142,13 +150,13 @@ function App() {
     return moves;
   };
 
-  const getLegalMoves = (pieceColor, pieceType, row, col) => {
+  const getPseudoLegalMoves = (pieceColor, pieceType, row, col, board, currentEnPassantTarget) => {
     const possibleMoves = []
     const directionMultiplier = pieceColor === "white" ? -1 : 1
 
     switch (pieceType) {
       case "pawn": {
-        const movesDone = chessBoard[row][col][1];
+        const movesDone = board[row][col][1];
         const forward1 = [row + 1 * directionMultiplier, col];
         const forward2 = [row + 2 * directionMultiplier, col];
         const diagonalLeft = [row + 1 * directionMultiplier, col - 1];
@@ -156,84 +164,86 @@ function App() {
 
         if (
           isValidPosition(forward1[0], forward1[1]) &&
-          !chessBoard[forward1[0]][forward1[1]]
+          !board[forward1[0]][forward1[1]]
         ) {
           possibleMoves.push(forward1);
           if (
             movesDone === 0 &&
             isValidPosition(forward2[0], forward2[1]) &&
-            !chessBoard[forward2[0]][forward2[1]]
+            !board[forward2[0]][forward2[1]]
           ) {
             possibleMoves.push(forward2);
           }
         }
         if (
           isValidPosition(diagonalLeft[0], diagonalLeft[1]) &&
-          chessBoard[diagonalLeft[0]][diagonalLeft[1]]
+          board[diagonalLeft[0]][diagonalLeft[1]]
         ) {
-          if (getColor(diagonalLeft) !== pieceColor) {
+          if (getColor(diagonalLeft, board) !== pieceColor) {
             possibleMoves.push(diagonalLeft);
           }
         }
         if (
           isValidPosition(diagonalRight[0], diagonalRight[1]) &&
-          chessBoard[diagonalRight[0]][diagonalRight[1]]
+          board[diagonalRight[0]][diagonalRight[1]]
         ) {
-          if (getColor(diagonalRight) !== pieceColor) {
+          if (getColor(diagonalRight, board) !== pieceColor) {
             possibleMoves.push(diagonalRight);
           }
         }
-        if (enPassantTarget) {
-          const [epRow, epCol] = enPassantTarget;
-          if (epRow === row + directionMultiplier && Math.abs(epCol - col) === 1) {
-            possibleMoves.push([epRow, epCol]);
+        if (currentEnPassantTarget) {
+          const [epRow, epCol] = currentEnPassantTarget;
+          if (epRow === row + directionMultiplier && (epCol === col - 1 || epCol === col + 1)) {
+             if (isValidPosition(epRow, epCol) && !board[epRow][epCol]) {
+                 possibleMoves.push([epRow, epCol]);
+             }
           }
         }
         break;
       }
       case "rook":
         possibleMoves.push(
-          ...getSlidingMoves(row, col, pieceColor, ROOK_DIRECTIONS)
+          ...getSlidingMoves(row, col, pieceColor, ROOK_DIRECTIONS, board)
         );
         break;
       case "knight":
         possibleMoves.push(
-          ...getSteppingMoves(row, col, pieceColor, KNIGHT_OFFSETS)
+          ...getSteppingMoves(row, col, pieceColor, KNIGHT_OFFSETS, board)
         );
         break;
       case "bishop":
         possibleMoves.push(
-          ...getSlidingMoves(row, col, pieceColor, BISHOP_DIRECTIONS)
+          ...getSlidingMoves(row, col, pieceColor, BISHOP_DIRECTIONS, board)
         );
         break;
       case "queen":
         possibleMoves.push(
-          ...getSlidingMoves(row, col, pieceColor, QUEEN_DIRECTIONS)
+          ...getSlidingMoves(row, col, pieceColor, QUEEN_DIRECTIONS, board)
         );
         break;
       case "king": {
         possibleMoves.push(
-          ...getSteppingMoves(row, col, pieceColor, KING_OFFSETS)
+          ...getSteppingMoves(row, col, pieceColor, KING_OFFSETS, board)
         );
-        const movesDone = chessBoard[row][col][1];
+        const movesDone = board[row][col][1];
         if (movesDone === 0) {
-          const rookKingside = chessBoard[row][col + 3];
+          const rookKingside = board[row][col + 3];
           if (
-            !chessBoard[row][col + 1] &&
-            !chessBoard[row][col + 2] &&
+            !board[row][col + 1] &&
+            !board[row][col + 2] &&
             rookKingside &&
-            getType([row, col + 3]) === 'rook' &&
+            getType([row, col + 3], board) === 'rook' &&
             rookKingside[1] === 0
           ) {
             possibleMoves.push([row, col + 2]);
           }
-          const rookQueenside = chessBoard[row][col - 4];
+          const rookQueenside = board[row][col - 4];
           if (
-            !chessBoard[row][col - 1] &&
-            !chessBoard[row][col - 2] &&
-            !chessBoard[row][col - 3] &&
+            !board[row][col - 1] &&
+            !board[row][col - 2] &&
+            !board[row][col - 3] &&
             rookQueenside &&
-            getType([row, col - 4]) === 'rook' &&
+            getType([row, col - 4], board) === 'rook' &&
             rookQueenside[1] === 0
           ) {
             possibleMoves.push([row, col - 2]);
@@ -246,19 +256,132 @@ function App() {
     }
     return possibleMoves;
   }
+  
+  const simulateMove = (board, from, to, pieceType) => {
+    const newBoard = board.map(row => [...row]);
+    const [fromRow, fromCol] = from;
+    const [toRow, toCol] = to;
+    let newEnPassantTarget = null;
+    const pieceColor = getColor(from, board);
+    const directionMultiplier = pieceColor === "white" ? -1 : 1;
+
+    const pieceData = newBoard[fromRow][fromCol];
+    const newPieceData = [...pieceData];
+    newPieceData[1] += 1;
+    
+    newBoard[toRow][toCol] = newPieceData;
+    newBoard[fromRow][fromCol] = "";
+
+    if (pieceType === 'pawn') {
+      if (Math.abs(fromRow - toRow) === 2) {
+        newEnPassantTarget = [fromRow + directionMultiplier, fromCol];
+      }
+      if (enPassantTarget && toRow === enPassantTarget[0] && toCol === enPassantTarget[1]) {
+        newBoard[toRow - directionMultiplier][toCol] = "";
+      }
+    }
+    
+    if (pieceType === 'king') {
+      const moveDist = toCol - fromCol;
+      if (moveDist === 2) {
+        const rook = newBoard[fromRow][7];
+        newBoard[fromRow][5] = [...rook];
+        newBoard[fromRow][5][1] += 1;
+        newBoard[fromRow][7] = "";
+      } else if (moveDist === -2) {
+        const rook = newBoard[fromRow][0];
+        newBoard[fromRow][3] = [...rook];
+        newBoard[fromRow][3][1] += 1;
+        newBoard[fromRow][0] = "";
+      }
+    }
+    
+    return [newBoard, newEnPassantTarget];
+  };
+  
+  const isKingInCheck = (kingColor, board, currentEnPassantTarget) => {
+    let kingPos = null;
+    const opponentColor = kingColor === 'white' ? 'black' : 'white';
+
+    for (let r = 0; r < 8; r++) {
+      for (let c = 0; c < 8; c++) {
+        if (board[r][c] && getType([r, c], board) === 'king' && getColor([r, c], board) === kingColor) {
+          kingPos = [r, c];
+          break;
+        }
+      }
+      if (kingPos) break;
+    }
+
+    if (!kingPos) return false;
+
+    for (let r = 0; r < 8; r++) {
+      for (let c = 0; c < 8; c++) {
+        if (board[r][c] && getColor([r, c], board) === opponentColor) {
+          const pieceType = getType([r, c], board);
+          const moves = getPseudoLegalMoves(opponentColor, pieceType, r, c, board, currentEnPassantTarget);
+          for (const move of moves) {
+            if (move[0] === kingPos[0] && move[1] === kingPos[1]) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  };
+  
+  const getLegalMoves = (pieceColor, pieceType, row, col, currentBoard, currentEnPassantTarget) => {
+    const pseudoMoves = getPseudoLegalMoves(pieceColor, pieceType, row, col, currentBoard, currentEnPassantTarget);
+    const legalMoves = [];
+
+    for (const move of pseudoMoves) {
+      const [tempBoard, tempEnPassantTarget] = simulateMove(currentBoard, [row, col], move, pieceType);
+      if (!isKingInCheck(pieceColor, tempBoard, tempEnPassantTarget)) {
+        legalMoves.push(move);
+      }
+    }
+    return legalMoves;
+  };
+  
+  const doesPlayerHaveLegalMoves = (playerColor, board, currentEnPassantTarget) => {
+    for (let r = 0; r < 8; r++) {
+      for (let c = 0; c < 8; c++) {
+        if (board[r][c] && getColor([r, c], board) === playerColor) {
+          const pieceType = getType([r, c], board);
+          const moves = getLegalMoves(playerColor, pieceType, r, c, board, currentEnPassantTarget);
+          if (moves.length > 0) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  };
 
   const handlePromotion = (pieceName) => {
     if (!promotionData) return;
 
-    const { color, row, col } = promotionData;
+    const { color, row, col, board } = promotionData;
 
     const newPieceIcon = pieces[color][pieceName].icon;
     const newPieceData = [newPieceIcon, 0];
 
-    const boardCopy = chessBoard.map(r => [...r]);
+    const boardCopy = board.map(r => [...r]);
     boardCopy[row][col] = newPieceData;
-    setChessBoard(boardCopy);
+    
+    const opponentColor = color === "white" ? "black" : "white";
+    const kingIsInCheck = isKingInCheck(opponentColor, boardCopy, null);
+    setInCheck(kingIsInCheck ? opponentColor : null);
+    
+    const opponentHasLegalMoves = doesPlayerHaveLegalMoves(opponentColor, boardCopy, null);
+    if (kingIsInCheck && !opponentHasLegalMoves) {
+      setWinner(color);
+    } else if (!kingIsInCheck && !opponentHasLegalMoves) {
+      setWinner("Stalemate");
+    }
 
+    setChessBoard(boardCopy);
     setPromotionData(null);
     setTurn(color === "white" ? "black" : "white");
     setEnPassantTarget(null);
@@ -267,92 +390,65 @@ function App() {
   const pieceClick = (key) => {
     const row = parseInt(key[0]);
     const col = parseInt(key[2]);
-    const boardCopy = chessBoard.map(row => [...row]);
-    const blackPiecesCopy = [...blackPieces];
-    const whitePiecesCopy = [...whitePieces];
 
     if (selectedPiece.length !== 0) {
       if (isLegal(row, col)) {
         const [pieceType, [fromRow, fromCol]] = selectedPiece;
         const selectedColor = getColor([fromRow, fromCol]);
-        const directionMultiplier = selectedColor === "white" ? -1 : 1;
-        let isEnPassant = false;
         let isKingCapture = false;
 
-        if (pieceType === "pawn" && enPassantTarget &&
-          row === enPassantTarget[0] && col === enPassantTarget[1]) {
-
-          isEnPassant = true;
-          const capturedPawnRow = row - directionMultiplier;
-          const capturedPiece = boardCopy[capturedPawnRow][col];
-
-          if (capturedPiece) {
-            const targetColor = getColor([capturedPawnRow, col]);
-            if (targetColor === "white") {
-              whitePiecesCopy.push(capturedPiece[0]);
-              setWhitePieces(whitePiecesCopy);
-            } else {
-              blackPiecesCopy.push(capturedPiece[0]);
-              setBlackPieces(blackPiecesCopy);
-            }
-            boardCopy[capturedPawnRow][col] = "";
-          }
-        }
-
         const destinationPiece = chessBoard[row][col];
-        if (destinationPiece && !isEnPassant) {
-          const targetColor = getColor([row, col]);
-          if (selectedColor !== targetColor) {
-            if (getType([row, col]) === 'king') {
-              isKingCapture = true;
+        if (destinationPiece && getType([row, col]) === 'king') {
+            isKingCapture = true;
+        }
+
+        const [newBoard, newEnPassantTarget] = simulateMove(chessBoard, [fromRow, fromCol], [row, col], pieceType);
+
+        if (destinationPiece) {
+            const targetColor = getColor([row, col]);
+            if (selectedColor !== targetColor) {
+                const capturedPiece = chessBoard[row][col][0];
+                if (targetColor === "white") {
+                    setWhitePieces([...whitePieces, capturedPiece]);
+                } else {
+                    setBlackPieces([...blackPieces, capturedPiece]);
+                }
             }
-            if (targetColor === "white") {
-              whitePiecesCopy.push(destinationPiece[0]);
-              setWhitePieces(whitePiecesCopy);
+        } else if (pieceType === 'pawn' && enPassantTarget && row === enPassantTarget[0] && col === enPassantTarget[1]) {
+            const capturedPawnRow = row - (selectedColor === 'white' ? -1 : 1);
+            const capturedPiece = chessBoard[capturedPawnRow][col][0];
+            const targetColor = getColor([capturedPawnRow, col]);
+             if (targetColor === "white") {
+                setWhitePieces([...whitePieces, capturedPiece]);
             } else {
-              blackPiecesCopy.push(destinationPiece[0]);
-              setBlackPieces(blackPiecesCopy);
+                setBlackPieces([...blackPieces, capturedPiece]);
             }
-          }
         }
-
-        boardCopy[fromRow][fromCol][1] += 1;
-        boardCopy[row][col] = boardCopy[fromRow][fromCol];
-        boardCopy[fromRow][fromCol] = "";
-
-        if (pieceType === 'king') {
-          const moveDist = col - fromCol;
-          if (moveDist === 2) {
-            const rook = boardCopy[row][7];
-            boardCopy[row][5] = rook;
-            boardCopy[row][7] = "";
-            boardCopy[row][5][1] += 1;
-          } else if (moveDist === -2) {
-            const rook = boardCopy[row][0];
-            boardCopy[row][3] = rook;
-            boardCopy[row][0] = "";
-            boardCopy[row][3][1] += 1;
-          }
-        }
-
+        
         if (pieceType === 'pawn' && (row === 0 || row === 7) && !isKingCapture) {
-          setPromotionData({ color: selectedColor, row, col });
-          setChessBoard(boardCopy);
+          setPromotionData({ color: selectedColor, row, col, board: newBoard });
+          setChessBoard(newBoard);
           setLegalMove([]);
           setSelectedPiece([]);
           return;
         }
+        
+        const opponentColor = selectedColor === "white" ? "black" : "white";
+        const kingIsInCheck = isKingInCheck(opponentColor, newBoard, newEnPassantTarget);
+        setInCheck(kingIsInCheck ? opponentColor : null);
+        
+        const opponentHasLegalMoves = doesPlayerHaveLegalMoves(opponentColor, newBoard, newEnPassantTarget);
+        if (kingIsInCheck && !opponentHasLegalMoves) {
+            setWinner(selectedColor);
+        } else if (!kingIsInCheck && !opponentHasLegalMoves) {
+            setWinner("Stalemate");
+        }
 
-        setChessBoard(boardCopy);
+        setChessBoard(newBoard);
         setLegalMove([]);
         setSelectedPiece([]);
-        setTurn(selectedColor === "white" ? "black" : "white");
-
-        if (pieceType === "pawn" && Math.abs(fromRow - row) === 2) {
-          setEnPassantTarget([fromRow + (row - fromRow) / 2, fromCol]);
-        } else {
-          setEnPassantTarget(null);
-        }
+        setTurn(opponentColor);
+        setEnPassantTarget(newEnPassantTarget);
         return;
       } else {
         setLegalMove([]);
@@ -365,7 +461,7 @@ function App() {
       const pieceType = getType([row, col]);
 
       if (pieceColor === turn && !winner) {
-        const moves = getLegalMoves(pieceColor, pieceType, row, col);
+        const moves = getLegalMoves(pieceColor, pieceType, row, col, chessBoard, enPassantTarget);
         setLegalMove(moves);
         setSelectedPiece([pieceType, [row, col]]);
       }
@@ -406,7 +502,7 @@ function App() {
       {winner && (
         <div className="absolute inset-0 bg-black bg-opacity-80 flex justify-center items-center z-10">
           <div className="bg-neutral-800 p-10 rounded-lg flex flex-col items-center gap-6 text-white shadow-2xl">
-            <h3 className="text-4xl font-bold">{winner} Wins!</h3>
+            <h3 className="text-4xl font-bold">{winner === 'Stalemate' ? 'Stalemate!' : `${winner.toUpperCase()} Wins!`}</h3>
             <button
               onClick={resetGame}
               className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white font-bold rounded-lg shadow-lg transition-colors"
@@ -433,6 +529,11 @@ function App() {
             chessRow.map((cell, colIndex) => {
               const isSelected = selectedPiece[1] && selectedPiece[1][0] === rowIndex && selectedPiece[1][1] === colIndex;
               const isMoveLegal = isLegal(rowIndex, colIndex);
+              
+              let isChecked = false;
+              if (cell && getType([rowIndex, colIndex]) === 'king' && getColor([rowIndex, colIndex]) === inCheck) {
+                isChecked = true;
+              }
 
               return (
                 <li
@@ -440,6 +541,7 @@ function App() {
                   className={`w-full h-full flex items-center justify-center relative cursor-pointer
                     ${(colIndex + rowIndex) % 2 === 0 ? "bg-stone-100" : "bg-green-700"}
                     ${isSelected ? "bg-yellow-300" : ""}
+                    ${isChecked ? "bg-red-500" : ""}
                   `}
                   onClick={() => pieceClick(`${rowIndex}-${colIndex}`)}
                 >
@@ -483,3 +585,4 @@ function App() {
 }
 
 export default App
+
