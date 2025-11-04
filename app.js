d2f77f583f09f879436ab5968def883bd5efac78
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-11-04 12:52:37 +0000

    newDeployBuild

--- app.js
+++ app.js
@@ -7,22 +7,30 @@ const middleware = require('./utils/middleware')
 const gameRouter = require('./controllers/game')
 
 const app = express()
+
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res, next) => {
+  logger.info('--- incoming request ---')
+  logger.info('Method:', req.method, 'Path:', req.path)
+  logger.info('Headers:', JSON.stringify(req.headers))
+  logger.info('Parsed body:', req.body)
+  next()
+})
+
+app.use(middleware.requestLogger)
+
+app.use('/api/game', gameRouter)
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => logger.info('connected to MongoDB'))
   .catch((error) => logger.error('error connection to MongoDB:', error.message))
-
-
-app.use(middleware.requestLogger)
-app.use(express.json())
-
-app.use('/api/game', gameRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 module.exports = app
