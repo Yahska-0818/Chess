af4e25e32608ac1be9b9e33d117633b544f93972
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-11-04 13:15:33 +0000

    newDeployBuild

--- app.js
+++ app.js
@@ -7,30 +7,29 @@ const middleware = require('./utils/middleware')
 const gameRouter = require('./controllers/game')
 
 const app = express()
-
 app.use(express.json())
 
-app.use((req, res, next) => {
-  logger.info('--- incoming request ---')
-  logger.info('Method:', req.method, 'Path:', req.path)
-  logger.info('Headers:', JSON.stringify(req.headers))
-  logger.info('Parsed body:', req.body)
-  next()
-})
+app.use(express.static(path.join(__dirname, 'dist')));
 
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 app.use(middleware.requestLogger)
 
 app.use('/api/game', gameRouter)
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 logger.info('connecting to', config.MONGODB_URI)
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => logger.info('connected to MongoDB'))
-  .catch((error) => logger.error('error connection to MongoDB:', error.message))
 
 module.exports = app
