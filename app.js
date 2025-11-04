7e54a9a83813519430e2c34ead4546d74b142105
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-11-04 12:22:56 +0000

    newDeployBuild

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -8,20 +9,28 @@ const gameRouter = require('./controllers/game')
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
-
 mongoose
   .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error) => logger.error('error connection to MongoDB:', error.message))
 
-app.use('/Chess', express.static('dist'))
-app.use(express.json())
+
 app.use(middleware.requestLogger)
-app.use('/api/game', gameRouter);
+app.use(express.json())
+
+const distPath = path.join(__dirname, 'dist')
+app.use('/', express.static(distPath, { index: 'index.html' }))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'), (err) => {
+    if (err) {
+      req.app.locals.logger && req.app.locals.logger.error('sendFile error', err)
+      res.status(500).send('Internal Server Error')
+    }
+  })
+})
+
+app.use('/api/game', gameRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
