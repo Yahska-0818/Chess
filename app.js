a9670de5ddab43d79bf31351a3cd659be427fede
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-11-08 13:26:35 +0000

    newDeployBuild

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-const gameRouter = require('./controllers/game');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const gameRouter = require('./controllers/game')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-logger.info('connecting to', config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB');
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message);
-  });
+    logger.error('error connection to MongoDB:', error.message)
+  })
+app.use(middleware.requestLogger)
 
-app.use(middleware.requestLogger);
+app.use('/api/game', gameRouter)
 
-app.use('/api/game', gameRouter);
-
-app.get('*', (req, res, next) => {
-  if (req.path.startsWith('/api/')) return next();
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+logger.info('connecting to', config.MONGODB_URI)
+
+module.exports = app
