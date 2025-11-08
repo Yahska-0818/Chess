100d84237c121d48d7d3e5d1cd9a96285881f9e7
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-11-08 06:40:59 +0000

    azure build ready

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const gameRouter = require('./controllers/game')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const gameRouter = require('./controllers/game');
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+logger.info('connecting to', config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
-app.use(middleware.requestLogger)
+    logger.error('error connection to MongoDB:', error.message);
+  });
 
-app.use('/api/game', gameRouter)
+app.use(middleware.requestLogger);
 
-app.get('/', (req, res) => {
+app.use('/api/game', gameRouter);
+
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api/')) return next();
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-logger.info('connecting to', config.MONGODB_URI)
-
-module.exports = app
+module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const http = require('http');
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+const PORT = config.PORT || process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
